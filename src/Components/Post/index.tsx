19fee92df26cf68eb2e52e59b72b09fdbf0926b0
macs1756 <kindrativmaksum312@gmail.com>
2023-12-15 20:24:01 +0200

    add component Post and net for main page

---
+++ src/Components/Post/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+const Post = () => {
+  return(
+    <div className='flex flex-col basis-1/4 flex-grow'>
+      <div>image</div>
+
+      <div className='flex justify-between items-center pt-2'>
+          <h3>admin</h3>
+          <p>17.06,233</p>
+      </div>
+
+    </div>
+  )
+}
+
+
+export default Post
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
+import Post from '../../Components/Post';
 
-function Home(): JSX.Element {
+ const Home = () =>  {
+
   return (
-    <div>Home</div>
-  );
+
+    <div className='max-w-[900px] mx-auto py-10'>
+      
+      <div className="flex justify-between items-start gap-8">
+
+        <div className='flex flex-col basis-4/5'>
+        <div className='text-xs uppercase text-white'>Posts</div>
+        <Post />
+        </div>
+
+        <div className="basis-1/5">
+          <div className='text-xs uppercase text-white'>Popular posts</div>
+          post
+        </div>
+        
+
+      </div>
+    </div>
+
+  )
 }
 
-export default Home;
+export default Home
