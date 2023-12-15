ebcea2c6499fc028254868cf19716cf81f61b7a5
macs1756 <kindrativmaksum312@gmail.com>
2023-12-15 15:53:31 +0200

    redux end

--- src/Redux/postSlice.ts
+++ src/Redux/postSlice.ts
@@ -3,17 +3,47 @@ import axios from "../utils/axios"
 import { IinitialStatePost } from '../Types'
 
 const initialState: IinitialStatePost = {
-    posts: [],
-    popularPosts: [],
-    isLoading: false,
+  posts: [],
+  popularPosts: [],
+  isLoading: false,
 }
+
+export const createPost = createAsyncThunk('post/createPost', async (params) => {
+  try {
+
+    const { data } = await axios.post('/posts/create', params)
+
+    return data
+
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 export const postSlice = createSlice({
   name: 'post',
   initialState,
   reducers: {},
   extraReducers(builder) {
-    
+
+    //Create post
+    builder.addCase(createPost.pending, (state) => {
+      state.isLoading = true
+    })
+
+
+    builder.addCase(createPost.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.posts.push(action.payload)
+    })
+
+
+    builder.addCase(createPost.rejected, (state) => {
+      state.isLoading = false
+    })
+
+
+
   },
 })
 
