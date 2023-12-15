c2f4b504203e673d18f9ff994948b6365b90deb8
macs1756 <kindrativmaksum312@gmail.com>
2023-12-15 15:17:26 +0200

    add part logic "add post"

--- src/Pages/AddPost/index.tsx
+++ src/Pages/AddPost/index.tsx
@@ -2,7 +2,22 @@ import * as React from 'react';
 
 function AddPost(): JSX.Element {
   return (
-    <div>Add post</div>
+    <form className='w-1/3 mx-auto py-10 ' onSubmit={(e)=>{ e.preventDefault() }}>
+
+      <div className='relative cursor-pointer'>
+        <label className='text-grey-300 py-2 bg-gray-600 text-xs  flex items-center justify-center border-2 border-dotted'>Add image:</label>
+        <input type="file" className='absolute top-0 left-0 w-full h-full  cursor-pointer  opacity-0' />
+      </div>
+   
+      <div className='flex object-cover py-2'>Image</div>
+
+      <label className='text-xs text-white opacity-70'>Title your post</label>
+
+      <input 
+      type="text"
+      
+       />
+    </form>
   );
 }
 
