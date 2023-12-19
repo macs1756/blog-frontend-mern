5d37157b8a90c1687b12b3d0d43ce2159b48e4a0
macs1756 <kindrativmaksum312@gmail.com>
2023-12-19 18:00:58 +0200

    change my popular posts

--- src/Components/PopularPost/index.tsx
+++ src/Components/PopularPost/index.tsx
@@ -4,11 +4,12 @@ import { IpropsComponentPost } from '../../Types'
 
 export const PopularPost = ({e}: IpropsComponentPost) => {
   return(
+      <div>
       <Link to={"/post/" + e?._id}> 
         <button className='bg-gray-600 my-1 cursor-pointer text-left'>
         <span className='flex text-xs p-2 text-gray-300 hover:bg-gray-800 hover:text-white transition-all'>{e.title}</span>
         </button>
-       
       </Link>
+      </div>
   )
 }
