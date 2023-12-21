b5577f97f8350613ff3a7db16f87e12024345544
macs1756 <kindrativmaksum312@gmail.com>
2023-12-21 13:49:24 +0200

    add avatar

--- src/Components/Comment/index.tsx
+++ src/Components/Comment/index.tsx
@@ -2,8 +2,17 @@ import * as React from 'react'
 import { IcommentComponentProps } from '../../Types'
 
 const Comment = ({ cmt }:IcommentComponentProps) => {
+
+  const avatar 
   return(
-    <div>Comment</div>
+    <div className='flex gap-3 items-center'>
+        <div className='flex item-center justify-center shrink-0 rounded-full w-10 h-10 bg-blue-300 text-sm'>
+          Avatar
+        </div>
+        <div className='flex text-gray-300 text-[10px]'>
+          {cmt?.comment}
+        </div>
+    </div>
   )
 }
 
